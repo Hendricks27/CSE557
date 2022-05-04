45b54791318794787d4c7f336a5e25bb22a718b3
Hendricks27 <wenjinzhang95@gmail.com>
2022-05-04 17:32:07 -0500

    hmm

--- Final/build.sh
+++ Final/build.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-tag=$1
-if [ -z "$1" ]; then
-    echo "No tag is provided, NOT going to push to docker hub."
-    tag="TEST"
-fi
+tag="V0.0.1"
 
+docker build -t wenjin27/visfinal:$tag -t wenjin27/visfinal:latest ./
 
-cp ../../APIFramework.py ./APIFramework.py
-
-docker build -t wenjin/final:$tag -t wenjin/final:latest ./
-
-if [ "$tag" != "TEST" ]; then
-    docker push wenjin/final:$tag
-    docker push wenjin/final:latest
-fi
-
-
-rm -rf pygly pygly-scripts
-rm APIFramework.py
+docker push wenjin27/visfinal:$tag
+docker push wenjin27/visfinal:latest
 
 
 
